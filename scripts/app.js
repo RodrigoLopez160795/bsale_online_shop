0363f50d1627c50df9bf0ce2826fdf581067927f
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-18 17:11:20 -0500

    Add navbar template

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+import { Navbar } from './components/navbar.js';
+import { root } from './config.js';
+import DOMHandler from './dom-handler.js';
+
+function App() {
+  DOMHandler.load(Navbar(), root);
+}
+
+export default App;
---
+++ scripts/components/navbar.js
@@ -0,0 +1,7 @@
+export function Navbar() {
+  return `
+        <div>
+        This is the navbar
+        </div>
+    `;
+}
