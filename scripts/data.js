e9e86ff4562a44a4587dbb867d782cd58d94ab06
Rodrigo López <rodrigo.lopez.160795@hotmail.com>
2022-10-19 19:28:51 -0500

    Create categories data

---
+++ scripts/data.js
@@ -0,0 +1,9 @@
+export const categories = [
+  { id: 1, name: 'Bebida energetica' },
+  { id: 2, name: 'Pisco' },
+  { id: 3, name: 'Ron' },
+  { id: 4, name: 'Bebida' },
+  { id: 5, name: 'Snack' },
+  { id: 6, name: 'Cerveza' },
+  { id: 7, name: 'Vodka' },
+];
